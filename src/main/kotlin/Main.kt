e6828fa83b9b665bda4d05f2d37a8e29b998de80
Artur Dias <artur.dias.g@gmail.com>
2022-10-21 13:37:47 -0300

    criando a primeira classe com kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,24 @@
 fun main() {
-    println("Hello World!")
-    println("Ol[a Mundo!")
+    println("Bem-vindo ao Bytebank!")
+    val contaArtur = Conta()
+    contaArtur.titular = "Artur"
+    contaArtur.numero = 100
+    contaArtur.saldo = 1000.0
+    println(contaArtur.titular)
+    println(contaArtur.numero)
+    println(contaArtur.saldo)
+
+    val contaPat = Conta()
+    contaPat.titular = "Patricia"
+    contaPat.numero = 101
+    contaPat.saldo = 2000.0
+    println(contaPat.titular)
+    println(contaPat.numero)
+    println(contaPat.saldo)
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
 }
